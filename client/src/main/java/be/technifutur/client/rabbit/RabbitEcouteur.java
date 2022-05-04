2f3b6b83046ec45a715ad1be36ccf96443bcdb1f
ManonKerrels <92925675+ManonKerrels@users.noreply.github.com>
2022-05-04 12:23:20 +0200

    Commu Client > Facture

---
+++ client/src/main/java/be/technifutur/client/rabbit/RabbitEcouteur.java
@@ -0,0 +1,23 @@
+package be.technifutur.client.rabbit;
+
+import be.technifutur.client.model.Facture;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitEcouteur {
+
+    private final Logger logger = LoggerFactory.getLogger(RabbitEcouteur.class);
+
+    @RabbitListener(queues = "fact_queue")
+    public void receiveFacture(String message) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Facture f = mapper.readValue(message, Facture.class);
+        logger.info("FACTURE RECEIVED - " + f);
+    }
+
+}
--- facture/src/main/java/be/technifutur/facture/rabbit/RabbitSender.java
+++ facture/src/main/java/be/technifutur/facture/rabbit/RabbitSender.java
@@ -1,4 +1,35 @@
 package be.technifutur.facture.rabbit;
 
-public class RabbitSender {
+import be.technifutur.facture.model.Facture;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class RabbitSender implements InitializingBean {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public RabbitSender(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendFactureToClient(String json){
+        rabbitTemplate.convertAndSend("topic.reserv", "fact", json);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Facture f = new Facture(
+                50,
+                UUID.randomUUID()
+        );
+
+        ObjectMapper mapper = new ObjectMapper();
+        String fJson = mapper.writeValueAsString(f);
+        sendFactureToClient(fJson);
+    }
 }
